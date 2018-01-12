e1eb56135737c853fc854d616970601711736326
Duy Tu <tduy.2201@gmail.com>
2018-01-12 10:26:59 +0700

    Initial load

---
+++ test/first_spec.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe("A feature", () => {
+  describe("A scenario", () => {
+    it("does something expected", () => {
+      assert(true);
+    });
+  });
+});
