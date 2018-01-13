e363f9705decaf22c755776e5d0308f859dda27e
Duy Tu <tduy.2201@gmail.com>
2018-01-13 16:51:13 +0700

    membership application

--- test/first_spec.js
+++
@@ -1,9 +0,0 @@
-var assert = require("assert");
-
-describe("A feature", () => {
-  describe("A scenario", () => {
-    it("does something expected", () => {
-      assert(true);
-    });
-  });
-});
